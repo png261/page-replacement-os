d5013edacc53386adb4339f0f35951aa4368c5d9
png261 <nhphuong.code@gmail.com>
2025-04-24 21:15:36 +0700

    fix: algorithms

--- script.js
+++ script.js
@@ -15,146 +15,195 @@ const elements = {
 let refStringArray = elements.refString.value.split(elements.separator.value || " ");
 let frames = parseInt(elements.numOfFrames.value);
 
-const createEmptyFrame = () => ['-', '-'];
-
-const initializeFrames = (count) => Array(count).fill().map(createEmptyFrame);
+const EMPTY_FRAME = '-'
+const initializeFrames = (count) => Array(count).fill().map(() => [EMPTY_FRAME, EMPTY_FRAME]);
 
 const findFrameIndex = (frames, condition) => frames.findIndex(condition);
 
-const getMinValueIndex = (frames, accessor) =>
-    findFrameIndex(frames, (item) => item[1][accessor] === Math.min(...frames.map((f) => f[1][accessor])));
+const getMinValueIndex = (frames, accessor) => {
+    if (!frames.length) return -1;
 
-const getMaxValueIndex = (frames, accessor) =>
-    findFrameIndex(frames, (item) => item[1][accessor] === Math.max(...frames.map((f) => f[1][accessor])));
+    let minIndex = 0;
+    let minValue = frames[0][1][accessor];
 
-// Page Replacement Algorithms
+    for (let i = 1; i < frames.length; i++) {
+        const value = frames[i][1][accessor];
+        if (value < minValue) {
+            minValue = value;
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+};
+
+const getMaxValueIndex = (frames, accessor) => {
+    if (!frames.length) return -1;
+
+    let maxIndex = 0;
+    let maxValue = frames[0][1][accessor];
+
+    for (let i = 1; i < frames.length; i++) {
+        const value = frames[i][1][accessor];
+        if (value > maxValue) {
+            maxValue = value;
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+};
+
+const getFrameIndex = (frames, targetRef) => {
+    return frames.findIndex((item) => item[0] === targetRef)
+}
+
 const algorithms = {
-    FIFO: (prevFrame, ref, index, isFull) => {
-        const frame = [...prevFrame];
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    FIFO: (frames, ref, index) => {
+        const refIndex = getFrameIndex(frames, ref)
         if (refIndex === -1) {
-            const targetIndex = isFull
-                ? getMinValueIndex(frame, 1)
-                : findFrameIndex(frame, (item) => item[0] === '-');
-            frame[targetIndex] = [ref, index];
+            const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
+            const targetIndex = emptyIndex !== -1 ? emptyIndex : getMinValueIndex(frames, 1)
+            frames[targetIndex] = [ref, index];
         }
-        elements.frameSubInfo.innerHTML = '(first-in index)';
-        return frame;
     },
 
-    LRU: (prevFrame, ref, index, isFull) => {
-        const frame = [...prevFrame];
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    LRU: (frames, ref, index) => {
+        const refIndex = getFrameIndex(frames, ref)
         if (refIndex === -1) {
-            const targetIndex = isFull
-                ? getMinValueIndex(frame, 1)
-                : findFrameIndex(frame, (item) => item[0] === '-');
-            frame[targetIndex] = [ref, index];
+            const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
+            const targetIndex = emptyIndex !== -1 ? emptyIndex : getMinValueIndex(frames, 1)
+            frames[targetIndex] = [ref, index];
         } else {
-            frame[refIndex][1] = index;
+            frames[refIndex][1] = index;
         }
-        elements.frameSubInfo.innerHTML = '(least recent index)';
-        return frame;
     },
 
-    MRU: (prevFrame, ref, index, isFull) => {
-        const frame = [...prevFrame];
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    MRU: (frames, ref, index) => {
+        const refIndex = getFrameIndex(frames, ref)
         if (refIndex === -1) {
-            const targetIndex = isFull
-                ? getMaxValueIndex(frame, 1)
-                : findFrameIndex(frame, (item) => item[0] === '-');
-            frame[targetIndex] = [ref, index];
+            const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
+            const targetIndex = emptyIndex !== -1 ? emptyIndex : getMaxValueIndex(frames, 1)
+            frames[targetIndex] = [ref, index];
         } else {
-            frame[refIndex][1] = index;
+            frames[refIndex][1] = index;
         }
-        elements.frameSubInfo.innerHTML = '(most recent index)';
-        return frame;
     },
 
-    LFU: (prevFrame, ref, index, isFull) => {
-        const frame = [...prevFrame];
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    LFU: (frames, ref, index) => {
+        const refIndex = getFrameIndex(frames, ref)
+        const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
         if (refIndex === -1) {
-            if (!isFull) {
-                frame[findFrameIndex(frame, (item) => item[0] === '-')] = [ref, [1, index]];
+            if (emptyIndex !== -1) {
+                frames[emptyIndex] = [ref, [1, index]];
             } else {
-                const leastFrequent = frame.filter((item) => item[1][0] === Math.min(...frame.map((f) => f[1][0])));
+                const leastFrequent = frames.filter((item) => item[1][0] === Math.min(...frames.map((f) => f[1][0])));
                 const victim = leastFrequent.find((item) => item[1][1] === Math.min(...leastFrequent.map((f) => f[1][1])));
-                frame[findFrameIndex(frame, (item) => item[0] === victim[0])] = [ref, [1, index]];
+                frames[findFrameIndex(frames, (item) => item[0] === victim[0])] = [ref, [1, index]];
             }
         } else {
-            frame[refIndex][1][0]++;
+            frames[refIndex][1][0]++;
         }
-        elements.frameSubInfo.innerHTML = '(frequency, first-in index)';
-        return frame;
     },
 
-    MFU: (prevFrame, ref, index, isFull) => {
-        const frame = [...prevFrame];
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    MFU: (frames, ref, index, isFull) => {
+        const refIndex = getFrameIndex(frames, ref)
+        const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
         if (refIndex === -1) {
             if (!isFull) {
-                frame[findFrameIndex(frame, (item) => item[0] === '-')] = [ref, [1, index]];
+                frames[emptyIndex] = [ref, [1, index]];
             } else {
-                const mostFrequent = frame.filter((item) => item[1][0] === Math.max(...frame.map((f) => f[1][0])));
+                const mostFrequent = frames.filter((item) => item[1][0] === Math.max(...frames.map((f) => f[1][0])));
                 const victim = mostFrequent.find((item) => item[1][1] === Math.min(...mostFrequent.map((f) => f[1][1])));
-                frame[findFrameIndex(frame, (item) => item[0] === victim[0])] = [ref, [1, index]];
+                frames[findFrameIndex(frames, (item) => item[0] === victim[0])] = [ref, [1, index]];
             }
         } else {
-            frame[refIndex][1][0]++;
+            frames[refIndex][1][0]++;
         }
-        elements.frameSubInfo.innerHTML = '(frequency, first-in index)';
-        return frame;
     },
 
-    '2ND': (prevFrame, ref, index, isFull, frameCount) => {
-        const frame = [...prevFrame];
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    '2ND': (frames, ref, index, isFull, frameCount) => {
+        const refIndex = getFrameIndex(frames, ref)
+        const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
         if (refIndex === -1) {
             if (!isFull) {
-                frame[findFrameIndex(frame, (item) => item[0] === '-')] = [ref, [1, index]];
+                frames[emptyIndex] = [ref, [1, index]];
             } else {
-                let refPointer = getMinValueIndex(frame, 1);
-                while (frame[refPointer][1][0] === 1) {
-                    frame[refPointer][1][0] = 0;
+                let refPointer = getMinValueIndex(frames, 1);
+                while (frames[refPointer][1][0] === 1) {
+                    frames[refPointer][1][0] = 0;
                     refPointer = (refPointer + 1) % frameCount;
                 }
-                frame[refPointer] = [ref, [1, index]];
+                frames[refPointer] = [ref, [1, index]];
             }
         } else {
-            frame[refIndex] = [ref, [1, index]];
+            frames[refIndex] = [ref, [1, index]];
         }
-        elements.frameSubInfo.innerHTML = '(reference bit, first-in index)';
-        return frame;
     },
 
-    OPT: (prevFrame, ref, index, isFull) => {
-        const frame = prevFrame.map((item) => item[1][0] === '∞' ? [item[0], [Infinity, item[1][1]]] : item);
-        const refIndex = findFrameIndex(frame, (item) => item[0] === ref);
+    OPT: (frames, ref, index, isFull, frameCount) => {
+        const refIndex = getFrameIndex(frames, ref);
+        const emptyIndex = getFrameIndex(frames, EMPTY_FRAME)
+
         if (refIndex === -1) {
-            if (!isFull) {
-                frame[findFrameIndex(frame, (item) => item[0] === '-')] = [ref, [0, index]];
+            let targetIndex;
+            if (emptyIndex !== -1) {
+                targetIndex = emptyIndex;
             } else {
-                const longestDistance = frame.filter((item) => item[1][0] === Math.max(...frame.map((f) => f[1][0])));
-                const victim = longestDistance.find((item) => item[1][1] === Math.min(...longestDistance.map((f) => f[1][1])));
-                frame[findFrameIndex(frame, (item) => item[0] === victim[0])] = [ref, [0, index]];
+                let maxDistance = -1;
+                let maxDistanceIndex = 0;
+                let minInsertionIndex = Infinity;
+
+                for (let i = 0; i < frames.length; i++) {
+                    const [page, [distance, insertionIndex]] = frames[i];
+                    if (distance > maxDistance || (distance === maxDistance && insertionIndex < minInsertionIndex)) {
+                        maxDistance = distance;
+                        maxDistanceIndex = i;
+                        minInsertionIndex = insertionIndex;
+                    }
+                }
+                targetIndex = maxDistanceIndex;
+            }
+
+            frames[targetIndex] = [ref, [distanceToNextRef(index + 1, ref), index]];
+        } else {
+            frames[refIndex][1] = [distanceToNextRef(index + 1, ref), frames[refIndex][1][1]];
+        }
+
+        for (let i = 0; i < frames.length; i++) {
+            if (frames[i][0] !== EMPTY_FRAME) {
+                frames[i][1][0] = distanceToNextRef(index + 1, frames[i][0]);
             }
         }
-        for (let j = 0; j < frame.length; j++) {
-            if (frame[j][0] !== '-') {
-                frame[j][1][0] = distanceToNextRef(index + 1, frame[j][0]) === Infinity ? '∞' : distanceToNextRef(index + 1, frame[j][0]);
-            }
-        }
-        elements.frameSubInfo.innerHTML = '(distance with next string reference item, first-in index)';
-        return frame;
     },
+};
+
+const getFrameSubInfo = (algorithmName) => {
+    switch (algorithmName) {
+        case 'FIFO':
+            return '(first-in index)';
+        case 'LRU':
+            return '(least recent index)';
+        case 'MRU':
+            return '(most recent index)';
+        case 'LFU':
+            return '(frequency, first-in index)';
+        case 'MFU':
+            return '(frequency, first-in index)';
+        case '2ND':
+            return '(reference bit, first-in index)';
+        case 'OPT':
+            return '(distance with next string reference item, first-in index)';
+        default:
+            return '';
+    }
 };
 
 const visualize = (refs, frameCount, algo) => {
     let pageFaults = 0;
-    let currentFrame = initializeFrames(frameCount);
+    const currentFrame = initializeFrames(frameCount);
     elements.visualPageFrames.innerHTML = '';
+    elements.frameSubInfo.innerHTML = getFrameSubInfo(algo)
 
     // Initialize table structure
     let tableHTML = '<table class="table-auto border-collapse border border-primary w-full text-center">';
@@ -168,10 +217,9 @@ const visualize = (refs, frameCount, algo) => {
 
     refs.forEach((ref, i) => {
         const isFull = findFrameIndex(currentFrame, (item) => item[0] === '-') === -1;
-        const prevFrame = [...currentFrame];
-        const refIndex = findFrameIndex(prevFrame, (item) => item[0] === ref);
+        const refIndex = findFrameIndex(currentFrame, (item) => item[0] === ref);
 
-        currentFrame = algorithms[algo](prevFrame, ref, i, isFull, frameCount);
+        algorithms[algo](currentFrame, ref, i, isFull, frameCount);
 
         const isPageFault = refIndex === -1;
         if (isPageFault) pageFaults++;
